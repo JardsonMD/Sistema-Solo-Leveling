2773d45e152b4c1b9df74bafb1f17917e6f34fbc
Jardson <101016178+JardsonMD@users.noreply.github.com>
2026-01-19 23:13:35 -0300

    Add files via upload

--- funcoes.py
+++ funcoes.py
@@ -23,7 +23,8 @@ def reset_diario():
     
     if dados.get("ultima_data") != hoje:
         for missao in dados["missoes"]:
-            dados["missoes"][missao] = False
+            if dados["missoes"][missao]["tipo"] == 1:
+                dados["missoes"][missao]["feita"] = False
 
         dados["ultima_data"] = hoje
         salvar_dados(dados)
@@ -32,13 +33,17 @@ def reset_diario():
 
 # ========== XP ========== 
 
+# Calcula o Nivel do Jogador
+def calc_nivel(xp):
+    return xp // 100 + 1
+
 
 # Carrega o XP do arquivo json
 def carregar_xp():
     try:
         with open(ARQUIVO, "r", encoding= "utf-8") as f:
             dados = json.load(f)
-            return dados.get("xp", 0)
+            return dados["player"]["xp"]
     except FileNotFoundError:
         return 0
 
@@ -46,9 +51,17 @@ def carregar_xp():
 
 # Mostra o Menu para o usuário
 def mostrar_menu():
-    print(">=====Sistema Solo Leveling =====<")
-    print("Seja Bem Vindo Jogador.")
 
+    with open(ARQUIVO, "r", encoding = "utf-8") as f:
+        dados = json.load(f)
+
+    xp = dados["player"]["xp"]
+    nivel = calc_nivel(xp)
+    xp_proximo_nivel = nivel * 100
+
+    print(">=====Sistema Solo Leveling =====<")
+    print(f"Jogador - Nivel {nivel} | XP - {xp} / {xp_proximo_nivel}" )
+    print()
     print("1 - Ver Missões Diarias")
     print("2 - Concluir Missões")
     print("3 - Ver XP")
@@ -63,8 +76,8 @@ def mostrar_missoes():
             dados = json.load(f)
         
         print("Missões Diárias")
-        for missao, feita in dados["missoes"].items():
-            status = "O" if feita else "X"
+        for missao in dados["missoes"]:
+            status = "O" if dados["missoes"][missao]["feita"] else "X"
             print(f"- {missao} [{status}]")
     
     except FileNotFoundError:
@@ -76,11 +89,11 @@ def concluir_missao(nome_missao):
         dados = json.load(f)
     
     if nome_missao in dados["missoes"]:
-        if not dados["missoes"][nome_missao]:
-            dados["missoes"][nome_missao] = True
-            dados["xp"] += 50
+        if not dados["missoes"][nome_missao]["feita"]:
+            dados["missoes"][nome_missao]["feita"] = True
+            dados["player"]["xp"] += dados["missoes"][nome_missao]["xp"]
             salvar_dados(dados)
-            print("Missão concluida. +50 XP.")
+            print("Missão concluida. + ", dados["missoes"][nome_missao]["xp"], "XP.")
         else:
             print("Essa missão já foi concluída.")
     else:
